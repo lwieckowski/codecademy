adb40542efb8ff1ddd454e163dea3ef59e482e5d
lwieckowski <luukka.wieckowski@gmail.com>
2021-03-31 12:17:48 +0200

    Finish mixed messages project

--- full-stack-path/portfolio/mixed-messages/app.js
+++ full-stack-path/portfolio/mixed-messages/app.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+const locations = ['a dark room', 'a dark forest', 'a deserted village',
+		   'an abandoned house', 'an abandoned hospital'];
+
+const companions = ['strangers', 'soldiers', 'the walking dead', 'your friends'];
+
+const items = ['gun', 'eye', 'leg', 'arm', 'tooth', 'nail', 'foot'];
+
+
+const pickRandom = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const generateMessage = () => {
+    const part1 = `You wake up in ${pickRandom(locations)}, `;
+    const part2 = `surrounded by ${pickRandom(companions)}. `;
+    const part3 = `You notice that your ${pickRandom(items)} is gone. `;
+    const part4 = `What do you do?`;
+    return part1 + part2 + '\n' + part3 + '\n' + part4
+}
+
+console.log(generateMessage());
